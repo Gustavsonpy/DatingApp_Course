dbe3b7954ecdd54aa92a4b4985a2877d4ed5096f
Gustavo <gupaluski@gmail.com>
2026-01-05 21:59:15 -0300

    Add DTO

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bb5212bab84848d5651797b41677951beb118ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae8c55ab16ac4765b801220b8bb270a3d3ab61eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
